26c54c892c44b5dbca6c19e5c70de39c2a096824
Amr ElHusseiny <amroashram@hotmail.com>
2022-08-14 17:35:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,20 +4,23 @@ from setuptools import setup, find_packages
 
 VERSION = '0.0.1' 
 DESCRIPTION = 'Plot Network using python , and output to DrawIO file'
-LONG_DESCRIPTION = 'Plot Network using python , and output to DrawIO file'
+
+with open('README.md') as readme_file:
+       LONG_DESCRIPTION = readme_file.read()
 
 # Setting up
 setup(
        # the name must match the folder name 'verysimplemodule'
         name="drawio_network_plot", 
-        version="0.0.1",
-        author="Jason Dsouza",
-        author_email="<youremail@email.com>",
+        version="0.0.02 Beta",
+        author="Amr ElHusseiny",
+        author_email="amroashram@hotmail.com",
         description=DESCRIPTION,
-        long_description=LONG_DESCRIPTION,
+        long_description= LONG_DESCRIPTION ,
+        long_description_content_type='text/markdown',
         packages=find_packages(),
         install_requires=[], # No packages needed - all standard libraries
         keywords=['python', 'first package'],
-        classifiers= ["No Classifier"],
+        classifiers= ['Development Status :: 4 - Beta','Topic :: System :: Networking'],
         python_requires='>=3.6',
 )
